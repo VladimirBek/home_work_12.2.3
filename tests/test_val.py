7822506026e70b2714d62681d0e21f4cef678cb9
VladimirBek <vladimirbek1996@gmail.com>
2023-04-27 14:07:35 +0300

    added fixture in test_val.py

--- tests/test_val.py
+++ tests/test_val.py
@@ -1,6 +1,10 @@
 from utils.dicts import get_val
-dic = {"vcs": "mercurial", 'lang': 'Python', 'school': 'Skypro'}
-def test_get_val():
-    assert get_val(dic, 'lang') == 'Python'
-    assert get_val(dic, 'other') == 'default value'
-    assert get_val(dic, 'other', 'Git') == 'Git'
+import pytest
+@pytest.fixture
+def dic_for_test_get_val():
+    return {"vcs": "mercurial", 'lang': 'Python', 'school': 'Skypro'}
+
+def test_get_val(dic_for_test_get_val):
+    assert get_val(dic_for_test_get_val, 'lang') == 'Python'
+    assert get_val(dic_for_test_get_val, 'other') == 'default value'
+    assert get_val(dic_for_test_get_val, 'other', 'Git') == 'Git'
